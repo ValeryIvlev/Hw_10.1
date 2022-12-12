1b6e93ce3971769e7e9a69e71abd37609842ac24
Valery <qaivlev@yandex.ru>
2022-12-12 22:26:15 +0300

    Test

--- src/test/java/org/Oman/SimpleTest.java
+++ src/test/java/org/Oman/SimpleTest.java
@@ -1,25 +1,53 @@
 package org.Oman;
 
-import org.junit.jupiter.api.*;
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTest {
 
-
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        Configuration.timeout = 60000;
+        Configuration.headless = false;
+        Configuration.webdriverLogsEnabled = true;
+        Configuration.browser = Browsers.CHROME;
+        open();
+    }
     @Test
     void fillFromTest() {
 
-
-
-            System.out.println("###      @Test firstTest() !");
-            assertTrue(3>2);
-
+        open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("Mail@mail.ru");
+        $("#gender-radio-1").doubleClick();
+        $("#userNumber").setValue("7999111123");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__day--004").click();
+        $("#subjectsInput").setValue("Economics").pressEnter();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#currentAddress").setValue("Subject one or no one:)");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
+        $("#submit").click();
+        $(".table-responsive").shouldHave(text("Ivan Ivanov"), text("Mail@mail.ru"), text("male"), text("7999111123"),
+                text("04 July,1993"), text("Economics"), text("Sports"), text("Subject one or no one:)"),
+                text("NCR Noida") );
+        $("#closeLargeModal").click();
 
     }
-
-    }
-
+}
