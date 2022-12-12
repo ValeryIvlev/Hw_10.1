648b86f4e7477b45dc420b73bd65dae0b3f8550b
Valery <qaivlev@yandex.ru>
2022-12-12 22:04:49 +0300

    Test

--- src/test/java/org/Oman/SimpleTest.java
+++ src/test/java/org/Oman/SimpleTest.java
@@ -1,10 +1,8 @@
 package org.Oman;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleTest {
